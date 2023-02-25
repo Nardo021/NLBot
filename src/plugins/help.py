17b076873ba51bd4e19500337db5853feebbd9d5
meowjiao <Meowjiao321@outlook.com>
2023-02-25 21:32:41 +0800

    update

--- src/plugins/help.py
+++ src/plugins/help.py
@@ -1,9 +1,10 @@
 from nonebot import on_command
+from nonebot.adapters.onebot.v11 import Bot
 
-help_text = "NLBot Help Menu"
+help_text = "NLBot 帮助菜单\nhelp: 获取帮助\n命令前缀: /"
 
 help = on_command("help", priority=0)
 
 @help.handle()
-async def handle_city():
+async def _(bot:Bot):
     await help.finish(help_text)
